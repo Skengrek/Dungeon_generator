92dedec9f03f51d1ced4080b88c7f9654cf4d8f3
Skengrek <armand.masse69@gmail.com>
2019-08-08 18:01:24 +0200

    Update and smal modification of class and main modules

--- src/Class.py
+++ src/Class.py
@@ -1,34 +1,68 @@
-import cv2
-import random
+"""This contains all class necessary to run the main code.
+    It contains :
+    Map
+    Room
+        Simple
+        Etc ...
+"""
 
-class Map(object):
-    # Room object. It define a Room with spetial properties
+import random
+import cv2
+
+
+class Map():
+    """ Map
+        This define the map object containing a list of Rooms objects.
+    """
+    # Map object. It define a Room with spetial properties
+
+    rooms_list = []
 
     def __init__(self, name):
         # Define the object Map.
         self.name = name
 
-    def addRoom(self,room_type):
-        if room_type=='Simple':
-            return Simple()
-            
+    def add_room(self, room_type):
+        """[summary]
 
-class Room(object):
-    def draw(self):
+        Arguments:
+            room_type {str} -- The definition of the new room type
+
+        Returns:
+            Room object -- Return the object created.
+        """
+        if room_type == 'Simple':
+            tmp_obj = Simple()
+            self.rooms_list.append(tmp_obj)
+            return tmp_obj
+        return None
+
+    def get_rooms_list(self):
+        """Get the list of rooms of this map object
+
+        Returns:
+            [Room] -- a table of Room object
+        """
+        return self.rooms_list
+
+
+class Room():
+    def draw(self, img):
         print('Not implemented')
 
     def fits(self):
-        print('Not Implemented') 
+        print('Not Implemented')
 
 
 class Simple(Room):
     # A simple Room is a basic rectangle with size and coordinate.
     def __init__(self):
-        self.x = random.randint(1,512)
-        self.y = random.randint(1,512)
-        self.hSize = random.randint(10,100)
-        self.vSize = random.randint(10,100)
+        self.x = random.randint(1, 512)
+        self.y = random.randint(1, 512)
+        self.hSize = random.randint(10, 100)
+        self.vSize = random.randint(10, 100)
 
     #! override
-    def draw(self,img):
-        cv2.rectangle(img,(self.x,self.y),(self.x+self.hSize,self.y+self.vSize),(0,255,0),2)
+    def draw(self, img):
+        cv2.rectangle(img, (self.x, self.y), (self.x+self.hSize,
+                                              self.y+self.vSize), (0, 255, 0), 2)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 # Hello
+import numpy as np
+import cv2
 from Class import Map
 
 
 mapObject = Map('New Map')
-obj = mapObject.addRoom('Simple')
+obj = mapObject.add_room('Simple')
 
-import cv2
-import numpy as np
 
 # Create a black image
-img = np.zeros((512,512,3), np.uint8)
+img = np.zeros((512, 512, 3), np.uint8)
 
 obj.draw(img)
 
-cv2.imshow('Image',img)
+cv2.imshow('Image', img)
 cv2.waitKey(0)
