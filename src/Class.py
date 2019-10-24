9f9a3cc688cbe5e2d0d27c326537f99a6424d206
Skengrek <armand.masse@cpe.fr>
2019-10-24 20:03:47 +0200

    Removed Pymunk to start adding a separation steering behavior

--- src/Class.py
+++ src/Class.py
@@ -2,13 +2,12 @@
 """
 
 from random import randint
-from math import pi, cos, sin, floor
+from math import pi, cos, sin, floor, isnan
 
 import logging
 
 import cv2
 import numpy as np
-import pymunk
 
 
 # ?                            Classes
@@ -40,7 +39,6 @@ class Map(object):
                 self.map_table[x].append(0)
         self.img = np.zeros((self.y_max, self.x_max, 3), np.uint8)
 
-        self.simulation_space = pymunk.Space()
 
         # ? Generation of the rooms
 
@@ -61,15 +59,13 @@ class Map(object):
             x_length = randint(self.x_length_min, self.x_length_max)
             y_length = randint(self.y_length_min, self.y_length_max)
 
-            self.rooms_list.append(Room(x, y, x_length, y_length,
-                                        self.simulation_space))
+            tmp_room = Room(x, y, x_length, y_length)
+            self.rooms_list.append(tmp_room)
 
     def room_separation(self):
         """ Use pymunk librairy to separate the rooms"""
 
-        for i in range(100):
-            self.simulation_space.step(0.01)
-            self.draw_map(0)
+        self.draw_map(0)
         # ? setup param in simulation
 
     def get_rooms_list(self):
@@ -103,18 +99,16 @@ class Map(object):
 class Room(object):
     """ A basic rectangle room"""
 
-    def __init__(self, x, y, x_len, y_len, simulation):
+    def __init__(self, x, y, x_len, y_len):
+        self.x = x
+        self.y = y
         self.x_len = x_len
         self.y_len = y_len
         self.weight = x_len * y_len
-        self.body = pymunk.Body(self.weight, 0)
-        self.body.position = x + x_len/2, y + y_len/2
-
-        # ? definition of the shape of the object :
-        points = [(x, y), (x + x_len, y),
-                  (x, y + y_len), (x + x_len, y + y_len)]
-
-        self.shape = pymunk.Poly(self.body, points)
+        self.corner1 = (self.x, self.y)
+        self.corner2 = (self.x + self.x_len, self.y + self.y_len)
+        self.color = (0, 255, 0)
+        self.border_size = 2
 
     def draw(self, img):
         """draw the room on the map
@@ -122,8 +116,7 @@ class Room(object):
         Arguments:
             img {OpenCV image} -- the openCV image where the room will be drawn
         """
-        x, y = self.body.position
-        x = floor(x)
-        y = floor(y)
-        cv2.rectangle(img, (x, y),
-                      (x+self.x_len, y+self.y_len), (0, 255, 0), 2)
+
+        cv2.rectangle(img, self.corner1, self.corner2,
+                      self.color, self.border_size)
+
