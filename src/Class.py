5dfb29e05bbbd5606615ec3ef8d7f5e0ae123026
Skengrek <armand.masse@cpe.fr>
2019-11-01 22:11:03 +0100

    WIP fixing the steering separation algorithm

--- src/Class.py
+++ src/Class.py
@@ -2,7 +2,7 @@
 """
 
 from random import randint
-from math import pi, cos, sin, floor, isnan
+from math import pi, cos, sin, floor, sqrt
 
 import logging
 
@@ -14,7 +14,7 @@ import numpy as np
 # *############################################################################
 
 
-def two_room_movement(room_1, room_2):
+def two_room_movement(room_1, room_2, x_middle, y_middle):
     """
     This function will test if two rooms overlap and will move the second one
 
@@ -28,12 +28,19 @@ def two_room_movement(room_1, room_2):
     x2, y2, len_x2, len_y2 = room_2.x, room_2.y, room_2.x_len, room_2.y_len
 
     # ? calculate overlaping area
-    dx = min(x1 + len_x1, x2 + len_x2) - max(x1, x2)
-    dy = min(y1 + len_y1, y2 + len_y2) - max(y1, y2)
+    dx = min(floor(x1 - len_x1 / 2), floor(x2 - len_x2 / 2)) \
+        - max(floor(x1 + len_x1 / 2), floor(x2 + len_x2 / 2))
+    dy = min(floor(y1 - len_y1 / 2), floor(y2 - len_y2 / 2)) \
+        - max(floor(y1 + len_y1 / 2), floor(y2 + len_y2 / 2))
+
+    room_1_dist = sqrt((x1 - x_middle)*(x1 - x_middle)
+                       + (y1 - y_middle)*(y1 - y_middle))
+    room_2_dist = sqrt((x2 - x_middle) * (x2 - x_middle)
+                       + (y2 - y_middle) * (y2 - y_middle))
 
     if (dx >= 0) and (dy >= 0):
-        move_unit = 1
-        if x1 > x2:
+        move_unit = 2
+        """if x1 > x2:
             move_x = - move_unit
         else:
             move_x = move_unit
@@ -41,8 +48,21 @@ def two_room_movement(room_1, room_2):
         if y1 > y2:
             move_y = - move_unit
         else:
-            move_y = move_unit
-        return [move_x, move_y]
+            move_y = move_unit"""
+
+        room_to_move = 1
+        x_force = x1 - x_middle
+        y_force = y1 - y_middle
+
+        if room_1_dist < room_2_dist:
+            room_to_move = 2
+            x_force = x2 - x_middle
+            y_force = y2 - y_middle
+        ref = min(x_force, y_force)
+        if ref == 0 :
+            ref = 1
+        move_x, move_y = floor(x_force), floor(y_force)
+        return [move_x, move_y, room_to_move]
     else:
         return False
 
@@ -60,8 +80,8 @@ class Map(object):
 
     rooms_list = []
     map_table = []
-    x_max = 920
-    y_max = 920
+    x_max = 512
+    y_max = 512
     x_length_max = 50
     x_length_min = 10
     y_length_max = 50
@@ -89,7 +109,7 @@ class Map(object):
 
         for i in range(nb_rooms):
             rand_angle = randint(0, 359)*180/pi
-            rand_dist = randint(0, radius)
+            rand_dist = randint(10, radius)
 
             x = floor(rand_dist * cos(rand_angle) + self.x_max/2)
             y = floor(rand_dist * sin(rand_angle) + self.y_max/2)
@@ -106,14 +126,22 @@ class Map(object):
         while room_moved:
             room_moved = False
             for base_room in self.rooms_list:
+                base_room.set_color("blue")
                 for move_room in self.rooms_list:
                     if base_room.id != move_room.id:
-                        result = two_room_movement(base_room, move_room)
+                        result = two_room_movement(base_room, move_room,
+                                                   floor(self.x_max / 2),
+                                                   floor(self.y_max / 2))
                         if result is not False:
-                            move_room.x += result[0]
-                            move_room.y += result[1]
+                            if result[2] == 2:
+                                move_room.x += result[0]
+                                move_room.y += result[1]
+                            else:
+                                base_room.x += result[0]
+                                base_room.y += result[1]
                             room_moved = True
-            self.draw_map(20)
+                self.draw_map(20)
+                base_room.set_color("green")
         # ? setup param in simulation
         print("Over")
 
@@ -140,6 +168,9 @@ class Map(object):
             wait_time {int} -- the time to wait for each update (0 means to wait for an input)
         """
         self.img = np.zeros((self.y_max, self.x_max, 3), np.uint8)
+        cv2.rectangle(self.img, (0, 0),
+                      (floor(self.x_max/2), floor(self.y_max/2)),
+                      (0, 0, 255), 2)
         for room in self.rooms_list:
             room.draw(self.img)
         cv2.imshow('Map', self.img)
@@ -159,14 +190,24 @@ class Room(object):
         self.color = (0, 255, 0)
         self.border_size = 2
 
+    def set_color(self, name):
+        if name == "green":
+            self.color = (0, 250, 0)
+        elif name == "blue":
+            self.color = (255, 0, 0)
+        elif name == "red:":
+            self.color = (0, 0, 250)
+
     def draw(self, img):
         """draw the room on the map
 
         Arguments:
             img {OpenCV image} -- the openCV image where the room will be drawn
         """
-        corner1 = (self.x, self.y)
-        corner2 = (self.x + self.x_len, self.y + self.y_len)
+        corner1 = (floor(self.x - self.x_len / 2),
+                   floor(self.y - self.y_len / 2))
+        corner2 = (floor(self.x + self.x_len / 2),
+                   floor(self.y + self.y_len / 2))
         cv2.rectangle(img, corner1, corner2,
                       self.color, self.border_size)
 
